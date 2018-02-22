3ee32bf5ddd1bd2ceedc22a612b31f7742d66c97
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-02-22 17:30:27 +0100

    Replace Empty ASP.NET Core MVC Project with Basic Visual Studio Template

---
+++ TasKing/TasKing/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TasKing.Domain.Abstract;
+using TasKing.Domain.Entities;
+using TasKing.Models;
+
+namespace TasKing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            repository.AddUser("DawidWika");
+
+            string ans = "";
+            foreach(var user in repository.Users)
+            {
+                ans += user.Email;
+            }
+
+            ViewData["Message"] = ans;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = repository.Users.ElementAt(0).Email;
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
