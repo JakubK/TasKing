b8a99f89580522f08a2bdee1ef62eb2214cc833c
Radek <radek511@op.pl>
2018-02-28 19:47:43 +0100

    Do some little css code refactor and few changes in js and html code

--- Front end views/src/js/task.js
+++ Front end views/src/js/task.js
@@ -1,40 +1,3 @@
-
-function editElement() {
-  const tasksList = document.querySelector(".tasks-list");
-  const checkbox = document.querySelector(".task-item #checkbox1").checked;
-  const taskDescription = document.querySelectorAll(".task-item .task-description");
-  const taskEditWrapper = document.querySelectorAll(".task-item-edit");
-  const itemList = document.querySelectorAll(".task-item-box");
-  const input = document.querySelectorAll(".task-item .task-input");
-  const cancelButton = document.querySelectorAll(".task-item .btn-decline");
-  const editButton = document.querySelectorAll(".task-item .btn-add");
-  const deleteButton = document.querySelectorAll(".task-item .btn-delete");
-
-  for (let i = 0; i < itemList.length; i++) {
-    itemList[i].addEventListener("click", () => {
-      taskEditWrapper[i].classList.add("visible");
-      itemList[i].classList.add("hidden");
-    });
-
-    cancelButton[i].addEventListener("click", () => {
-      itemList[i].classList.remove("hidden");
-      taskEditWrapper[i].classList.remove("visible");
-    });
-
-    input[i].value = taskDescription[i].innerHTML;
-
-    editButton[i].addEventListener("click", () => {
-      taskDescription[i] .innerHTML = input[i].value;
-      itemList[i].classList.remove("hidden");
-      taskEditWrapper[i].classList.remove("visible");
-    });
-
-    deleteButton[i].addEventListener("click", () => {
-      tasksList.removeChild(tasksList.childNodes[i]);
-    });
-  }
-} editElement();
-
 function addElement() {
   const addButton = document.querySelector(".add-task .btn-task-item");
   const cancelButton = document.querySelector("#cancel-task");
@@ -70,11 +33,51 @@ function addElement() {
 
     const newTask = document.createElement("li");
 
-    document.querySelector(".tasks-list").appendChild(newTask).innerHTML = '<li class="task-item"> <div class="task-item-box"> <input type="checkbox" id="checkbox1"> <label for="checkbox1"> <span class="fa fa-check" aria-hidden="true"></span> </label> <p class="task-description">'+ input.value +'</p> </div> <div class="task-item-edit"> <div class="task-item-edit-input"> <input class="task-input" type="text"> </div> <div class="task-item-edit-buttons"> <button class="btn-task-item btn-add">Zapisz</button> <button class="btn-task-item btn-decline">Anuluj</button></div></div></li>';
+    document.querySelector(".tasks-list").appendChild(newTask).innerHTML = '<li class="task-item"> <div class="task-item-box"> <input class="task-checkbox" type="checkbox" id="checkbox1"> <label for="checkbox1"> <span class="fa fa-check" aria-hidden="true"></span> </label> <p class="task-description">'+ input.value +'</p> </div> <div class="task-item-edit"> <div class="task-item-edit-input"> <input class="task-input" type="text"> </div> <div class="task-item-edit-buttons"> <button class="btn-task-item btn-add">Zapisz</button> <button class="btn-task-item btn-decline">Anuluj</button> <button class="btn-task-item btn-delete"><span class="fa fa-trash-o"></span></button> </div> </div> </li>';
     input.value = "";
   });
 
 } addElement();
+
+function editElement() {
+  const tasksList = document.querySelector(".tasks-list");
+  const checkbox = document.querySelectorAll(".task-item label");
+  const taskDescription = document.querySelectorAll(".task-item .task-description");
+  const taskEditWrapper = document.querySelectorAll(".task-item-edit");
+  const itemList = document.querySelectorAll(".task-item-box");
+  const input = document.querySelectorAll(".task-item .task-input");
+  const cancelButton = document.querySelectorAll(".task-item .btn-decline");
+  const editButton = document.querySelectorAll(".task-item .btn-add");
+  const deleteButton = document.querySelectorAll(".task-item .btn-delete");
+
+  for (let i = 0; i < itemList.length; i++) {
+    taskDescription[i].addEventListener("click", () => {
+      taskEditWrapper[i].classList.add("visible");
+      itemList[i].classList.add("hidden");
+    });
+
+    cancelButton[i].addEventListener("click", () => {
+      itemList[i].classList.remove("hidden");
+      taskEditWrapper[i].classList.remove("visible");
+    });
+
+    input[i].value = taskDescription[i].innerHTML;
+
+    editButton[i].addEventListener("click", () => {
+      taskDescription[i] .innerHTML = input[i].value;
+      itemList[i].classList.remove("hidden");
+      taskEditWrapper[i].classList.remove("visible");
+    });
+
+    deleteButton[i].addEventListener("click", () => {
+      tasksList.removeChild(tasksList.childNodes[i]);
+    });
+
+    checkbox[i].addEventListener("click", () => {
+      taskDescription[i].classList.toggle("checked");
+    });
+  }
+} editElement();
 
 function sortPanel() {
   const sortButton = document.querySelector(".board-settings .fa-sort");
